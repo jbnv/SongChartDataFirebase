a980556a99fe987ee232badd583654a06ca7994a
Jay Bienvenu <github@bienv.com>
2016-10-07 05:36:31 -0500

    Fix compile-geo.

--- app/compilers/compile-geo.js
+++ app/compilers/compile-geo.js
@@ -1,68 +1,89 @@
-var firebase    = require("firebase"),
-    chalk       = require("chalk"),
+var chalk       = require("chalk"),
     util        = require("gulp-util");
     numeral     = require("numeral"),
 
-    // meta        = require('../meta'),
     scoring     = require('../scoring');
 
-// entities: array of entities of the type
-module.exports = function() {
+require("../polyfill");
+
+var _inputs = {
+  "locations": "geo/raw",
+  "artists": "artists/compiled"
+}
+
+var _outputs = [
+  ["entities", "locations/compiled"],
+  ["titles", "locations/titles"],
+  ["errors", "locations/errors"]
+];
+
+function _transform(snapshot) {
+
   util.log(chalk.magenta("compile-geo.js"));
 
-  var db = firebase.database();
+  var locations = snapshot[0].val() || {};
+  var artists = snapshot[1].val() || {};
 
-  return firebase.Promise.all([
-    db.ref("genres/raw").once('value'),
-    db.ref("artists/by-genre").once('value'),
-    db.ref("songs/by-genre").once('value'),
-    db.ref("artists/compiled").once('value')
-  ])
+  entities = {};
+  titles = {};
+  errors = [];
 
-//
-//   util.log("Caching artists and scores...");
-//   var locationArtists = {};
-//   var locationScores = {};
-//   meta.getArtists().forEach(function(artist) {
-//     var slug = artist.origin;
-//     if (!locationArtists[slug]) { locationArtists[slug] = []; }
-//     if (!locationScores[slug]) { locationScores[slug] = 0.0; }
-//     locationArtists[slug].push(artist);
-//     if (artist.score) {
-//       try {
-//         locationScores[slug] += parseFloat(artist.score);
-//       } catch(error) {
-//       }
-//     }
-//   });
-//   util.log("Cache complete.");
-//
-//   entities.forEach(function(entity) {
-//     var slug = entity.instanceSlug;
-//
-//     titles[slug] = entity.title;
-//
-//     entity.artists = locationArtists[slug] || [];
-//     entity.score = locationScores[slug];
-//
-//     if (entity.artists && entity.artists.length > 0) {
-//       entity.artistAdjustedAverage = scoring.adjustedAverage(entity.score,entity.artists.length);
-//     }
-//
-//     numeral.zeroFormat("");
-//
-//     util.log(
-//       chalk.blue(entity.instanceSlug),
-//       entity.title,
-//       chalk.gray(numeral(entity.artists.length).format("0")),
-//       chalk.gray(numeral(entity.score || 0).format("0.00")),
-//       chalk.gray(numeral(entity.artistAdjustedAverage || 0).format("0.00"))
-//     );
-//
-//   });
-//
-//   return {
-//     "all": entities,
-//     "titles": titles,
-//   }
-// }
+  util.log("Caching artists and scores...");
+  var locationArtists = {};
+  var locationScores = {};
+  for (var artistSlug in artists) {
+    var artist = artists[artistSlug];
+    var slug = artist.origin;
+    if (!locationArtists[slug]) { locationArtists[slug] = []; }
+    if (!locationScores[slug]) { locationScores[slug] = 0.0; }
+    locationArtists[slug].push(artist);
+    if (artist.score) {
+      try {
+        locationScores[slug] += parseFloat(artist.score);
+      } catch(error) {
+      }
+    }
+  }
+  util.log("Cache complete.");
+
+  for (var slug in locations) {
+    var entity = locations[slug];
+
+    titles[slug] = entity.title;
+
+    var entityArtists = locationArtists[slug] || [];
+    entity.artists = entityArtists.toObject(
+      function(o) { return o.instanceSlug; },
+      function(o) { return o.title; }
+    );
+    entity.score = locationScores[slug] || 0;
+    entity.artistAdjustedAverage = entityArtists.adjustedAverage();
+
+    numeral.zeroFormat("");
+
+    util.log(
+      chalk.blue(entity.instanceSlug),
+      entity.title,
+      chalk.gray(numeral((entity.artists || []).length).format("0")),
+      chalk.gray(numeral(entity.artistAdjustedAverage || 0).format("0.00"))
+    );
+
+    entities[slug] = entity;
+
+  }
+
+  return {
+    "geo/compiled": entities,
+    "geo/titles": titles,
+    "geo/errors": errors
+  }
+
+}
+
+module.exports = {
+  singular: "location",
+  plural: "locations",
+  inputs: _inputs,
+  outputs: _outputs,
+  transform: _transform
+}
