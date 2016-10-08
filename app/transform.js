6fff9a2574b23aa826fdc4bd230e8777db1dc2b1
Jay Bienvenu <github@bienv.com>
2016-10-08 18:16:17 -0500

    Add byList() and expand().

--- app/transform.js
+++ app/transform.js
@@ -63,3 +63,35 @@ exports.sortAndRank = function(list,options) {
 
   return outbound;
 }
+
+function _title(v) {
+  if (!v) return null;
+  if (/boolean|number|string/.test(typeof mixed_var)) return v;
+  // assume object
+  return v.title || true;
+}
+
+// used in generating by-lists
+exports.byList = function (outbound,itemSlug,collection) {
+  if (!outbound) return {};
+  if (!collection) return outbound;
+  for (var key in collection) {
+    if (!outbound[key]) outbound[key] = {};
+    outbound[key][itemSlug] = _title(collection[key]);
+  }
+  return outbound;
+}
+
+// trueArray: { : true }
+// source: {}
+// transform: (optional) function to transform source object
+exports.expand =  function(trueArray,source,transform) {
+  if (!trueArray) return null;
+  if (!source) return null;
+  if (!transform) transform = function(x) { return x; };
+  var outbound = {};
+  for (var key in trueArray) {
+    outbound[key] = transform(source[key]) || true;
+  }
+  return outbound;
+}
