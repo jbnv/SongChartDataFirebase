062bd9c3efcc9d9e2099cbdaaa71b718f3c217b0
Jay Bienvenu <github@bienv.com>
2016-10-14 14:13:04 -0500

    Complete genre compiler.

--- app/compilers/compile-genre.js
+++ app/compilers/compile-genre.js
@@ -1,14 +1,9 @@
-var chalk       = require("chalk"),
-    util        = require("gulp-util");
-    numeral     = require("numeral"),
-
-    scoring     = require('../scoring');
-
 var _inputs = {
   "genres": "genres/raw",
   "artistsByGenre": "artists/by-genre",
   "songsByGenre": "songs/by-genre",
-  "artists": "artists/compiled"
+  "artists": "artists/compiled",
+  "songs": "songs/raw"
 }
 
 var _outputs = [
@@ -19,46 +14,44 @@ var _outputs = [
 
 function _transform(snapshot) {
 
-  util.log(chalk.magenta("compile-genre.js"));
+  var chalk       = require("chalk"),
+      util        = require("gulp-util"),
 
-  // var inputData = snapshot.reduce(function(s) {
-  //   return s.val() || {};
-  // })
+      display     = require('../display'),
+      scoring     = require('../scoring'),
+      transform   = require('../transform');
+
+  util.log(chalk.magenta("compile-genre.js"));
 
   var genres = snapshot[0].val() || {};
   var artistsByGenre = snapshot[1].val() || {};
   var songsByGenre = snapshot[2].val() || {};
-  var artists = snapshot[3].val() || {};
+  var allArtists = snapshot[3].val() || {};
+  var allSongs = snapshot[4].val() || {};
 
   entities = {};
   titles = {};
-  errors = [];
+  errors = {};
 
   for (var slug in genres) {
+
     var entity = genres[slug];
 
     titles[slug] = entity.title;
 
-    entity.artists =
-      (artistsByGenre[slug] || [])
-      .map(function(a) { return ""+a.instanceSlug; })
-      .reduce(function(outbound,key) {
-        outbound[key] = artists[key]; return outbound;
-      },{});
+    entity.artists = scoring.expandAndScore(artistsByGenre[slug],allArtists);
 
-    // entity.songs = scoring.sortAndRank(songsByGenre[entity.instanceSlug]) || [];
-    // scoring.scoreCollection.call(entity);
+    entity.songs = scoring.expandAndScore(songsByGenre[slug],allSongs);
 
-    numeral.zeroFormat("");
+    scoring.scoreCollection.call(entity);
 
     util.log(
-      chalk.blue(entity.instanceSlug),
+      chalk.blue(slug),
       entity.title,
-      chalk.gray(numeral((entity.songs || []).length).format("0")),
-      chalk.gray(numeral((entity.artists || []).length).format("0")),
-      chalk.gray(numeral(entity.score || 0).format("0.00")),
-      chalk.gray(numeral(entity.songAdjustedAverage || 0).format("0.00")),
-      chalk.gray(numeral(entity.artistAdjustedAverage || 0).format("0.00"))
+      display.count(entity.songs),
+      display.count(entity.artists),
+      display.number(entity.songAdjustedAverage),
+      display.number(entity.artistAdjustedAverage)
     );
 
     entities[slug] = entity;
@@ -78,5 +71,7 @@ module.exports = {
   plural: "genres",
   inputs: _inputs,
   outputs: _outputs,
-  transform: _transform
+  transform: _transform,
+  entities: "genres/compiled",
+  errors: "genres/errors"
 }
