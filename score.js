98652fa004fc55d0cf36befa500160d8adf41b19
Jay Bienvenu <github@bienv.com>
2016-11-02 10:45:57 -0500

    Implement infrastructure and swap.

--- score.js
+++ score.js
@@ -4,9 +4,8 @@ var chalk       = require("chalk"),
     util        = require("gulp-util"),
     yargs       = require('yargs'),
 
-    fbConfig    = require("../firebase-config"),
+    fbConfig    = require("./firebase-config"),
 
-    data        = require('./app/data'),
     scoring     = require('./app/scoring');
 
 require('./app/polyfill');
@@ -43,45 +42,6 @@ function unaryWithModifier(task,fn) {
   }
 }
 
-function swap(pair) {
-
-  var a = pair.split(",")[0];
-  var b = pair.split(",")[1];
-
-  var entityA = read(a);
-  var entityB = read(b);
-
-  // Strategy: Keep the peaks and ascent; scale the descents to swap the scores.
-
-  var ascentA = entityA["ascent-weeks"] || 1;
-  var ascentB = entityB["ascent-weeks"] || 1;
-
-  var descentA = entityA["descent-weeks"] || 1;
-  var descentB = entityB["descent-weeks"] || 1;
-
-  var scoreA = entityA.peak * (ascentA + descentA);
-  var scoreB = entityB.peak * (ascentB + descentB);
-
-  entityA["descent-weeks"] = scoreB / entityA.peak - ascentA;
-  entityB["descent-weeks"] = scoreA / entityB.peak - ascentB;
-
-  if (entityA["descent-weeks"] < 1)  entityA["descent-weeks"] = 1;
-  if (entityB["descent-weeks"] < 1)  entityB["descent-weeks"] = 1;
-
-  write(a,entityA);
-  write(b,entityB);
-
-  util.log(
-    chalk.green("swap"),
-    chalk.blue(a),
-    entityA.title
-  );
-  util.log(
-    chalk.green("swap"),
-    chalk.blue(b),
-    entityB.title
-  );
-}
 
 function interpolate(tuple) {
 
@@ -155,7 +115,7 @@ firebase.initializeApp(fbConfig.initConfig);
 firebase.auth().signInWithEmailAndPassword(fbConfig.email,fbConfig.password)
 .then(function() {
 
-  var data = require('./data')(firebase);
+  var data = require('./app/data')(firebase);
 
   return data.getBatch({
     "songs": "songs/raw",
@@ -165,86 +125,122 @@ firebase.auth().signInWithEmailAndPassword(fbConfig.email,fbConfig.password)
     var songs = snapshot[0].val() || {},
         artists = snapshot[1].val() || {},
 
-    processArguments("s",swap);
-    processArguments("i",interpolate);
-    // processArguments("c",clear);
-    // processArguments("z",zero);
-    processArguments("u",bendUp);
-    processArguments("d",bendDown);
+        db = firebase.database();
 
-    if (yargs.argv.all) {
+    function _write(slug,song) {
+      var ref = db.ref("songs/raw").child(slug);
+      ref.child("peak").set(song.peak);
+      ref.child("ascent-weeks").set(song["ascent-weeks"]);
+      ref.child("descent-weeks").set(song["descent-weeks"]);
+    }
 
-      var songs = meta.getSongs();
-      var artists = meta.getArtists();
-      var artistsTransformedCount = 0;
-      var songsTransformedCount = 0;
+    processArguments("s",
 
-      util.log("Processing",songs.length ,"by",artists.length,"artists.");
+      function(pair) {
 
-      var unscoredSongs = [];
-      var scoredSongCount = 0;
-      var totalScore = 0.0;
-      var ascent = [];
-      var totalDescentWeeks = 0.0;
+        var a = pair.split(",")[0];
+        var b = pair.split(",")[1];
 
-      songs.forEach(function(song) {
+        var swapped = scoring.swap(songs[a],songs[b])
 
-        if (song.ascent && !Array.isArray(song.ascent)) return;
+        _write(a,swapped[0]);
+        _write(b,swapped[1]);
 
-        if (!song.ascent || song.ascent.length == 0) {
-          unscoredSongs.push(song);
-          return;
-        }
+        util.log(
+          chalk.green("swap"),
+          chalk.blue(a),
+          songs[a].title,
+          chalk.gray(scoring.score(swapped[0]))
+        );
+        util.log(
+          chalk.green("swap"),
+          chalk.blue(b),
+          songs[b].title,
+          chalk.gray(scoring.score(swapped[1]))
+        );
+      }
+    );
 
-        for (i = 0; i < song.ascent.length; i++) {
-          if (ascent.length < i+1) {
-            ascent.push(song.ascent[i]);
-          } else {
-            ascent[i] += song.ascent[i];
-          }
-        }
+    // processArguments("i",interpolate);
+    //
+    // // processArguments("c",clear);
+    // // processArguments("z",zero);
+    //
+    // processArguments("u",bendUp);
+    //
+    // processArguments("d",bendDown);
 
-        scoredSongCount++;
-        totalDescentWeeks += song["descent-weeks"];
-
-      });
+    // if (yargs.argv.all) {
+    //
+    //   var artistsTransformedCount = 0;
+    //   var songsTransformedCount = 0;
+    //
+    //   util.log("Processing",songs.length ,"by",artists.length,"artists.");
+    //
+    //   var unscoredSongs = [];
+    //   var scoredSongCount = 0;
+    //   var totalScore = 0.0;
+    //   var ascent = [];
+    //   var totalDescentWeeks = 0.0;
+    //
+    //   songs.forEach(function(song) {
+    //
+    //     if (song.ascent && !Array.isArray(song.ascent)) return;
+    //
+    //     if (!song.ascent || song.ascent.length == 0) {
+    //       unscoredSongs.push(song);
+    //       return;
+    //     }
+    //
+    //     for (i = 0; i < song.ascent.length; i++) {
+    //       if (ascent.length < i+1) {
+    //         ascent.push(song.ascent[i]);
+    //       } else {
+    //         ascent[i] += song.ascent[i];
+    //       }
+    //     }
+    //
+    //     scoredSongCount++;
+    //     totalDescentWeeks += song["descent-weeks"];
+    //
+    //   });
 
       // Reduce ascent array to an average.
       // Truncate ascent to its
-      var peakValue = 0;
-      var peakIndex = 0;
+      // var peakValue = 0;
+      // var peakIndex = 0;
+      //
+      // ascent.forEach(function(e,index) {
+      //   var f = parseFloat(e) / scoredSongCount;
+      //   ascent[index] = f;
+      //   if (f > peakValue) { peakValue = f; peakIndex = index; }
+      // });
+      //
+      // ascent = ascent.slice(0,peakIndex+1);
+      // var descentWeeks = 1.0 * totalDescentWeeks / scoredSongCount;
+      // if (descentWeeks < 1) descentWeeks = 1;
+      // console.log("[231]",totalDescentWeeks,scoredSongCount);
+      //
+      // unscoredSongs.forEach(function(song) {
+      //   rawSong = read(song.instanceSlug);
+      //   rawSong.ascent = ascent;
+      //   rawSong["descent-weeks"] = descentWeeks;
+      //   write(song.instanceSlug,rawSong);
+      //
+      //   util.log(
+      //     chalk.gray("-"),
+      //     chalk.blue(song.instanceSlug),
+      //     song.title
+      //   );
+      // });
+      //
+      // util.log(
+      //   "Scored",
+      //   chalk.green(songsTransformedCount),
+      //   "unscored songs."
+      // );
 
-      ascent.forEach(function(e,index) {
-        var f = parseFloat(e) / scoredSongCount;
-        ascent[index] = f;
-        if (f > peakValue) { peakValue = f; peakIndex = index; }
-      });
-
-      ascent = ascent.slice(0,peakIndex+1);
-      var descentWeeks = 1.0 * totalDescentWeeks / scoredSongCount;
-      if (descentWeeks < 1) descentWeeks = 1;
-      console.log("[231]",totalDescentWeeks,scoredSongCount);
-
-      unscoredSongs.forEach(function(song) {
-        rawSong = read(song.instanceSlug);
-        rawSong.ascent = ascent;
-        rawSong["descent-weeks"] = descentWeeks;
-        write(song.instanceSlug,rawSong);
-
-        util.log(
-          chalk.gray("-"),
-          chalk.blue(song.instanceSlug),
-          song.title
-        );
-      });
-
-      util.log(
-        "Scored",
-        chalk.green(songsTransformedCount),
-        "unscored songs."
-      );
-
-    }
+    //}
   });
 
 });
