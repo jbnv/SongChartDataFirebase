ca44290ecf309232e7f8a79970b5e965fe56c48b
Jay Bienvenu <github@bienv.com>
2016-12-04 22:38:39 -0600

    Expand display with options/

--- app/display.js
+++ app/display.js
@@ -3,12 +3,25 @@ var chalk       = require("chalk"),
 
 numeral.zeroFormat("");
 
-function _wholeNumber(n) {
-  return chalk.gray(numeral(n || 0).format("0"));
+function _chooseColor(options) {
+  if (!options) return "gray";
+  if (options == "error") return "red";
+  if (options == "warning") return "yellow";
+  if (options.isError) return "red";
+  if (options.isWarning) return "yellow";
+  return "gray";
 }
 
-function _number(n) {
-  return chalk.gray(numeral(n || 0).format("0.00"));
+function _wholeNumber(n,options) {
+  if (!options) options = {};
+  var color = _chooseColor(options);
+  return chalk[color](numeral(n || 0).format("0") + (options.isCalculated ? "⎰" : ""));
+}
+
+function _number(n,options) {
+  if (!options) options = {};
+  var color = _chooseColor(options);
+  return chalk[color](numeral(n || 0).format("0.00") + (options.isCalculated ? "⎰" : ""));
 }
 
 exports.number = _number;
